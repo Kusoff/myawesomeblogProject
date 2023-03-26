d493057b1c951d2540f886ce40a36ec0489c916a
Kusov Alexandr <kusov.alex2004@mail.ru>
2023-03-26 17:27:19 +0300

    Add Blog, specific_post.html and urls

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -119,11 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'myawesomeblog/static/'
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/awesome_media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
